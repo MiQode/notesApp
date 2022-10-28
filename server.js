635cc21536dec1f4f4a1bbbd746df1589bfc3ff1
Mike Rembo <rembomike@gmail.com>
2022-10-28 13:22:33 +0300

    added logger and middleware

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 
-// builtin middleware
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+// built-in middleware
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
@@ -19,5 +30,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
